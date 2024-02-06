89c71a937c3d52efd1ad780d8f6a2efa6b88887c
coderEdu <gtpanteraxtr@gmail.com>
2024-02-06 09:50:11 -0300

    test point

---
+++ BEComentarios/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BEComentarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BEComentarios
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Comentario> Comentario { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- BEComentarios/Program.cs
+++ BEComentarios/Program.cs
@@ -1,4 +1,10 @@
+using BEComentarios;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 // Add services to the container.
 
